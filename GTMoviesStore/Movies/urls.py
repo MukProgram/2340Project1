361f5b9a2d9234ea178cd4cdcfd19ac8456b2019
MukProgram <mukeshkp2005@gmail.com>
2025-02-04 17:32:58 -0500

    Fixed repo. Got signup working.

--- GTMoviesStore/Movies/urls.py
+++ GTMoviesStore/Movies/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index, name='index'),
-    path('cart', views.cart, name = 'cart')
+    path('cart', views.cart, name = 'cart'),
+    path('about', views.about, name = 'about'),
+    path('movies', views.movies, name = 'movies')
 ]
