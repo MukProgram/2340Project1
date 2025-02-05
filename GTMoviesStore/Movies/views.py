f00599ec0856d65a99bb591c641be5a87409cfec
Ari Hammond <ajhammond06@gmail.com>
2025-02-04 20:35:23 -0500

    signup no longer loading, attempted reversion/troubleshooting.

--- GTMoviesStore/Movies/views.py
+++ GTMoviesStore/Movies/views.py
@@ -3,21 +3,21 @@ from .models import *
 
 def index(request):
     template_data = {}
-    template_data['title'] = "GT Movies Store"
+    template_data['title'] = 'GT Movies Store'
     movies = Movie.objects.all()
     return render(request, "Movies/index.html", {"movies": movies})
 
 def cart(request):
     template_data = {}
-    template_data['title'] = "Shopping Cart"
+    template_data['title'] = 'Shopping Cart'
     movies = Movie.objects.all()
     return render(request, "Movies/cart.html", {"movies": movies})
 def about(request):
     template_data = {}
-    template_data['title'] = "About"
+    template_data['title'] = 'About'
     return render(request, "Movies/about.html")
 def movies(request):
     template_data = {}
-    template_data['title'] = "Movie Library"
+    template_data['title'] = 'Movie Library'
     movies = Movie.objects.all()
     return render(request, "Movies/movies.html", {"movies": movies})
