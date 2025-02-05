043a435b60e5aff38a84ddc909e7c4e8465de3ce
Ari Hammond <ajhammond06@gmail.com>
2025-02-04 19:28:03 -0500

    login page and functionalities in navbar!

--- GTMoviesStore/Movies/views.py
+++ GTMoviesStore/Movies/views.py
@@ -2,14 +2,22 @@ from django.shortcuts import render
 from .models import *
 
 def index(request):
+    template_data = {}
+    template_data['title'] = "GT Movies Store"
     movies = Movie.objects.all()
     return render(request, "Movies/index.html", {"movies": movies})
 
 def cart(request):
+    template_data = {}
+    template_data['title'] = "Shopping Cart"
     movies = Movie.objects.all()
     return render(request, "Movies/cart.html", {"movies": movies})
 def about(request):
+    template_data = {}
+    template_data['title'] = "About"
     return render(request, "Movies/about.html")
 def movies(request):
+    template_data = {}
+    template_data['title'] = "Movie Library"
     movies = Movie.objects.all()
     return render(request, "Movies/movies.html", {"movies": movies})
