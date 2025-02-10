0d38ff4eb16fee007396ca6f9f91c68babb7ccce
Anvith Bekal <anvithnbekal@gmail.com>
2025-02-10 16:45:49 -0500

    Added deletion functionality for reviews

--- GTMoviesStore/Movies/urls.py
+++ GTMoviesStore/Movies/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('movies/<int:id>/', views.show, name='movies.show'),
     path('<int:id>/review/<int:review_id>/edit/',
         views.edit_review, name='movies.edit_review'),
+    path('<int:id>/review/<int:review_id>/delete/',
+         views.delete_review, name='movies.delete_review'),
 ]
 
