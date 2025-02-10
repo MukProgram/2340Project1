80194e532d5fa5733a56c5a7666a7220db5994c5
Anvith Bekal <anvithnbekal@gmail.com>
2025-02-10 09:01:48 -0500

    Created a branch and ability to edit reviews

--- GTMoviesStore/Movies/urls.py
+++ GTMoviesStore/Movies/urls.py
@@ -5,6 +5,8 @@ urlpatterns = [
     path('about', views.about, name = 'about'),
     path('movies', views.movies, name = 'movies'),
     path('<int:id>/review/create/', views.create_review, name = 'movies.create_review'),
-    path('movies/<int:id>/', views.show, name='movies.show')
+    path('movies/<int:id>/', views.show, name='movies.show'),
+    path('<int:id>/review/<int:review_id>/edit/',
+        views.edit_review, name='movies.edit_review'),
 ]
 
